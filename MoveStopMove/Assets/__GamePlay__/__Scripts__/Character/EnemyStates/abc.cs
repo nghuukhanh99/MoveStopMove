1e74fc357db851c3673324b306bba29316e12935
nghuukhanh99 <huukhanh99x@gmail.com>
2022-03-29 19:12:17 +0700

    a

--- MoveStopMove/Assets/__GamePlay__/__Scripts__/Character/EnemyStates/abc.cs
+++ MoveStopMove/Assets/__GamePlay__/__Scripts__/Character/EnemyStates/abc.cs
@@ -7,9 +7,9 @@ public class abc : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("OK");   
     }
-
+    
     // Update is called once per frame
     void Update()
     {
